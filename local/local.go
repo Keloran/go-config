2c909b0da8cce592ffc5236739f76626c662fbff
Keloran <keloran@develbox.info>
2024-04-24 16:31:22 +0100

    feat: Add method to fetch all environment variables
    Add a new method getAllEnvironment to fetch all environment variables and populate
    the EnvMap field in the Local struct. This allows for easy access to all environment
    variables within the application.

--- local/local.go
+++ local/local.go
@@ -1,16 +1,20 @@
 package local
 
 import (
+	"os"
+	"strings"
+
 	"github.com/bugfixes/go-bugfixes/logs"
 	"github.com/caarlos0/env/v8"
 )
 
 // Local is the local config
 type Local struct {
-	KeepLocal   bool `env:"BUGFIXES_LOCAL_ONLY" envDefault:"false"`
-	Development bool `env:"DEVELOPMENT" envDefault:"false"`
-	HTTPPort    int  `env:"HTTP_PORT" envDefault:"80"`
-	GRPCPort    int  `env:"GRPC_PORT" envDefault:"3000"`
+	KeepLocal   bool              `env:"BUGFIXES_LOCAL_ONLY" envDefault:"false"`
+	Development bool              `env:"DEVELOPMENT" envDefault:"false"`
+	HTTPPort    int               `env:"HTTP_PORT" envDefault:"80"`
+	GRPCPort    int               `env:"GRPC_PORT" envDefault:"3000"`
+	EnvMap      map[string]string `env:"ENV_MAP" envDefault:"{}"`
 }
 
 func NewLocal(local, dev bool, http, grpc int) *Local {
@@ -27,5 +31,19 @@ func Build() (*Local, error) {
 	if err := env.Parse(l); err != nil {
 		return l, logs.Errorf("failed to parse local config: %v", err)
 	}
+
+	l.getAllEnvironment()
+
 	return l, nil
+}
+
+func (l *Local) getAllEnvironment() {
+	envVars := make(map[string]string)
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		if len(pair) == 2 {
+			envVars[pair[0]] = pair[1]
+		}
+	}
+	l.EnvMap = envVars
 }
